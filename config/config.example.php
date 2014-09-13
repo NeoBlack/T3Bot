0d713fbbf393915d9fd1130a3bf00a8d6ebff35f
Frank Nägler <typo3@naegler.net>
2014-09-13 19:27:07 +0200

    fix typo

--- config/config.example.php
+++ config/config.example.php
@@ -1,6 +1,6 @@
 <?php
 
-$GLOBAL['config'] = array(
+$GLOBALS['config'] = array(
 	'slack'	=> array(
 		'outgoingWebhookToken' => '' // secret token, to secure the request was received from slack.com
 	)
