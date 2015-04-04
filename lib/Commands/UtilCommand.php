39c772de58e007873657dda38b0ae952cafe29ea
Frank Nägler <typo3@naegler.net>
2015-04-04 13:30:43 +0200

    [TASK] Optimize util:coin feature

--- lib/Commands/UtilCommand.php
+++ lib/Commands/UtilCommand.php
@@ -28,7 +28,14 @@ class UtilCommand extends AbstractCommand {
 		$params = $this->params;
 		array_shift($params);
 		$params = implode(' ', $params);
-		$options = explode(',', $params);
+		$options = array_map('trim', explode(',', $params));
+		if (count($options) === 1) {
+			return '*T3Bot says:* _A complicated decision ..._';
+		}
+		if (count(array_unique($options)) === 1) {
+			return '*T3Bot says:* _it is undecidable ..._';
+		}
+
 		$option = $options[rand(0, count($options)-1)];
 		return '*T3Bot says:* _' . $option . '_';
 	}
